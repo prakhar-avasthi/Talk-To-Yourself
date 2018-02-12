6b9fc2de8750ba2548918eb6901ff5862b597313
Prakhar Avasthi <prakhar.avasthi@stonybrook.edu>
2018-02-12 06:47:06 -0500

    Update flaskr.py

--- flaskr.py
+++ flaskr.py
@@ -4,11 +4,11 @@ from flask import Flask, request, session, g, redirect, url_for, abort, render_t
 from contextlib import closing
 
 # configuration
-DATABASE = 'D:\\Study\\5_coursera\\Flask\\flaskr\\tmp\\flaskr.db'
+DATABASE = './tmp//flaskr.db'
 DEBUG = True
 SECRET_KEY = 'qwertyuiop'
-USERNAME = 'admin'
-PASSWORD = 'default'
+USERNAME = '*********'
+PASSWORD = '*********'
 
 # create our little application :)
 app = Flask(__name__)
@@ -16,13 +16,13 @@ app.config.from_object(__name__)
 
 def connect_db():
     return sqlite3.connect(app.config['DATABASE'])
-    
+
 def init_db():
     with closing(connect_db()) as db:
         with app.open_resource('schema.sql', mode='r') as f:
             db.cursor().executescript(f.read())
         db.commit()
-        
+
 @app.before_request
 def before_request():
     g.db = connect_db()
@@ -32,13 +32,13 @@ def teardown_request(exception):
     db = getattr(g, 'db', None)
     if db is not None:
         db.close()
-        
+
 @app.route('/')
 def show_entries():
     cur = g.db.execute('select title, text from entries order by id desc')
     entries = [dict(title=row[0], text=row[1]) for row in cur.fetchall()]
     return render_template('show_entries.html', entries=entries)
-    
+
 @app.route('/add', methods=['POST'])
 def add_entry():
     if not session.get('logged_in'):
@@ -48,26 +48,25 @@ def add_entry():
     g.db.commit()
     flash('New entry was successfully posted')
     return redirect(url_for('show_entries'))
-    
+
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     error = None
     if request.method == 'POST':
-        if request.form['username'] != app.config['USERNAME']:
-            error = 'Invalid username'
-        elif request.form['password'] != app.config['PASSWORD']:
-            error = 'Invalid password'
+        if request.form['username'] != app.config['USERNAME'] or request.form['password'] != app.config['PASSWORD']:
+            error = 'Invalid username or password'
+            session['logged_in'] = False
         else:
             session['logged_in'] = True
             flash('You were logged in')
             return redirect(url_for('show_entries'))
     return render_template('login.html', error=error)
-    
+
 @app.route('/logout')
 def logout():
     session.pop('logged_in', None)
     flash('You were logged out')
     return redirect(url_for('show_entries'))
-    
+
 if __name__ == '__main__':
     app.run()
